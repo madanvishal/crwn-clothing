2fd58f967ad7dac8b0574e2326db6c9cc65733e8
Vishal Madan <VMadan@antal.com>
2019-12-22 21:55:39 +0530

    Added Preview Collection and Shop Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import {Route} from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
     <switch>
     <Route exact path='/' component={HomePage} />
-    <Route exact path='/hats' component={HatsPage} />
+    <Route exact path='/shop' component={ShopPage} />
     </switch>
     </div>
   );
