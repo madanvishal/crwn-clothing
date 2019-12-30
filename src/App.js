1d346f2f1d3470680d6074e5b6d3354b565a49ec
Vishal Madan <VMadan@antal.com>
2019-12-31 01:10:09 +0530

    Added Sign Up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import {Switch, Route} from 'react-router-dom';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -20,9 +20,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
   
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     // this.setState({currentUser: user});
+     if(userAuth) {
+       const userRef = await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapShot => {
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+             ...snapShot.data()
+           }
+         })   
+         
+         console.log(this.state);
+
+     })} else {
+      this.setState({currentUser: userAuth});
+     }   
     })
   }
 
